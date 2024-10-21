a942a3179056cf59874f89ae8984d154e6bd9d4e
mohammad shariful <shariful24jan@gmail.com>
2024-10-21 13:01:33 +0800

    Problem slove

---
+++ problem1.js
@@ -0,0 +1,71 @@
+//1.Write a function called addNumbers that takes two numbers as parameters and returns their sum.
+//2.Write a function called sumUpToN that takes a number n and returns the sum of all numbers from 1 to n. Use a loop.
+//3.Write a function called sumArray that takes an array of numbers as a parameter and returns the sum of all the numbers in the array.
+//4.Write a function called countEvenNumbers that takes an array of numbers as a parameter and returns the count of even numbers in the array.
+//5.Write a function called stringLength that takes a string as a parameter and returns the length of the string.
+
+
+
+// Problem-1:
+// let num1 = 10;
+// let num2 = 5;
+// function addNumbers(number1,number2){
+//       return number1 + number2;
+// }
+// console.log(addNumbers(num1,num2));
+
+
+
+// Problem-2:
+// let  num = 20;
+// let sum = 0;
+
+// function sumUpToN(n){
+//        for(i = 1; i <= n; i++){
+//               sum = sum + i;
+//        }
+//        return sum
+// } 
+
+// console.log(sumUpToN(num));
+
+
+// problem-3:
+ 
+// let num = [10,14,5,12]
+// let sum = 0;
+// function sumArray(a){
+//        for(i = 0; i < a.length; i++){
+//               sum = sum + a[i];
+//        }
+//           return sum;
+// }
+
+// console.log(sumArray(num));
+
+
+
+// problem-4:
+// let num = [10,13,15,8,14,25,18,20];
+// let sum = 0;
+// function countEvenNumbers(n){
+//        for(i = 0; i < n.length; i++){
+//        if(n[i] % 2 === 0){
+//        sum++
+//        }
+// }
+// return sum
+// }
+
+// console.log(countEvenNumbers(num));
+
+
+
+// problem-5:
+// let string = "Shariful islam";
+
+// function stringLength(s){
+//        length = s.length;
+//        return length;
+// }
+// console.log(stringLength(string));
